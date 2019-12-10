f626b11cb5f040dd06fa029a47ff826745eb344f
ammy <kaurkamaldeep14@gmail.com>
2019-12-10 18:28:25 -0500

    This is commit from GitDemo Project

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -1,14 +1,28 @@
 package qaclickacademy;
 
+import org.testng.annotations.Test;
+
 public class RestAPITest {
 
 	public RestAPITest() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		@Test
+		public void postJira()
+		{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+		}
 
-	}
+		@Test
+		public void deleteTwitter()
+		{
+		System.out.println("deleteTwitter");
+		}
+//	}
 
 }
